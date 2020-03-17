3d9a012d8bf58b0808bef9ce3036513fc431548e
helio <insign@gmail.com>
2020-03-17 00:05:35 -0300

    WIP: nginx

--- install.sh
+++ install.sh
@@ -28,6 +28,10 @@ command_exists() {
   command -v "$@" >/dev/null 2>&1
 }
 
+install() {
+  apt install -y "$@"
+}
+
 error() {
   echo "$RED""Error: $@""$RESET" >&2
   exit 1
@@ -57,28 +61,110 @@ others_checks() {
 }
 
 step_user_creation() {
-  if [ $(getent passwd "$user") ]; then
-    if [ "$AUTO" = "true" ] || [ "$DELETE_EXISTING_USER" = "true" ]; then
-      info Deleting current user: "$GREEN""$BOLD""$user""$RESET"
-      userdel -r $user
-      success Deleted.
-    else
-      error user already exists, use --delete-existing-user or --auto or choose another: "$GREEN""$BOLD""$user""$RESET"
+  if [ "$CREATE_NEW_USER" != "false" ]; then
+    if [ $(getent passwd "$user") ]; then
+      if [ "$AUTO" = "true" ] || [ "$DELETE_EXISTING_USER" = "true" ]; then
+        info Deleting current user: "$GREEN""$BOLD""$user""$RESET"
+        userdel -r $user
+        success Deleted.
+      else
+        error user already exists, use --delete-existing-user or --auto or choose another: "$GREEN""$BOLD""$user""$RESET"
+      fi
+    fi
+
+    useradd "$user" -m -p $(openssl passwd -1 "$pass") -s $(which zsh)
+    usermod -aG sudo "$user" # append to sudo and user group
+    success User created: "$BLUE""$BOLD""$user"
+  fi
+}
+
+step_ufw() {
+  if [ "$NO_UFW" != "true" ]; then
+    # TODO allow add ips via command
+    install ufw
+    ufw logging on
+    ufw default deny incoming
+    ufw default allow outgoing
+    ufw allow ssh
+    ufw allow 22
+    ufw allow 80
+    ufw allow 443
+
+    ufw enable
+    ufw status
+  fi
+}
+
+step_nginx() {
+  if [ "$NO_NGINX" != "true" ]; then
+    install nginx
+
+    cat >/etc/nginx/conf.d/gzip.conf <<EOF
+gzip on;
+gzip_comp_level 5;
+gzip_min_length 1000;
+gzip_proxied no-cache no-store private expired auth;
+gzip_vary on;
+gzip_types
+application/atom+xml
+application/javascript
+application/json
+application/rss+xml
+application/vnd.ms-fontobject
+application/x-font-ttf
+application/x-web-app-manifest+json
+application/xhtml+xml
+application/xml
+font/opentype
+image/svg+xml
+image/x-icon
+text/css
+text/plain
+text/x-component;
+EOF
+    service nginx restart
+
+    if command_exists ufw; then
+      ufw allow 'Nginx HTTP'
+      ufw allow 'Nginx HTTPS'
     fi
   fi
-
-  useradd "$user" -m -p $(openssl passwd -1 "$pass") -s $(which zsh)
-  usermod -aG sudo "$user" # append to sudo and user group
-  success User created: "$BLUE""$BOLD""$user"
+}
+step_php() {
+  # TODO Composer, php-fpm, laravel command
+  if [ "$NO_UFW" != "true" ]; then
+    echo ''
+  fi
+}
+step_node() {
+  # TODO node, npm, yarn
+  if [ "$NO_UFW" != "true" ]; then
+    echo ''
+  fi
+}
+step_mysql() {
+  if [ "$NO_UFW" != "true" ]; then
+    echo ''
+  fi
+}
+step_postgres() {
+  if [ "$NO_UFW" != "true" ]; then
+    echo ''
+  fi
+}
+step_lets_encrypt() {
+  if [ "$NO_UFW" != "true" ]; then
+    echo ''
+  fi
 }
 
 step_final() {
-  if [ "$NOOMZ" != "true" ]; then
+  if [ "$NO_OMZ" != "true" ]; then
     info Installing ohmyzsh...
     runuser -l $user -c 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended'
   fi
 
-  printf "$GREEN"
+  echo "$GREEN"
   # http://patorjk.com/software/taag/#p=display&f=ANSI%20Shadow&t=DONE!
   cat <<-'EOF'
 
@@ -91,7 +177,7 @@ step_final() {
       ╚═════╝  ╚═════╝ ╚═╝  ╚═══╝╚══════╝╚═╝
 
 	EOF
-  printf "$RESET"
+  echo "$RESET"
 
   echo "USER: $RED$BOLD$user$RESET and password: $RED$BOLD$pass$RESET" >&2
 
@@ -113,23 +199,15 @@ setup_color() {
 }
 
 setup_basics() {
-  info Update and Upgrade
-  apt update && apt upgrade -y
+  if [ "$SKIP_UPDATES" != "true" ]; then
+    info Update and Upgrade
+    apt update && apt upgrade -y
+  fi
   info Installing zsh and other basics...
-  apt install -y zsh curl wget software-properties-common locales
+  install zsh curl wget software-properties-common locales
 }
 
-main() {
-  setup_color
-  call_vars
-  others_checks
-  setup_basics
-
-  # Run as unattended if stdin is closed
-  if [ ! -t 0 ]; then
-    AUTO="true"
-  fi
-
+parse_arguments() {
   # Parse arguments
   while [ "$#" -gt 0 ]; do
     case "$1" in
@@ -146,12 +224,28 @@ main() {
       AUTO="true"
       shift 1
       ;;
+    --dont-create-new-user) # don't creates a new user (--auto creates)
+      CREATE_NEW_USER="false"
+      shift 1
+      ;;
     --delete-existing-user) # delete existent user if it exists (--auto deletes)
       DELETE_EXISTING_USER="true"
       shift 1
       ;;
+    --skip-updates) # do not updates nor upgrades the system (--auto does)
+      SKIP_UPDATES="true"
+      shift 1
+      ;;
     --no-ohmyzsh) # do not install oh-my-zsh framework (--auto installs)
-      NOOMZ="true"
+      NO_OMZ="true"
+      shift 1
+      ;;
+    --no-ufw) # do not install or configure UFW firewall (--auto does)
+      NO_UFW="true"
+      shift 1
+      ;;
+    --no-nginx) # do not install or configure nginx (--auto does)
+      NO_NGINX="true"
       shift 1
       ;;
     --user=*)
@@ -174,8 +268,25 @@ main() {
       ;;
     esac
   done
+}
+
+main() {
+  setup_color
+  call_vars
+  others_checks
+
+  # Run as unattended if stdin is closed
+  if [ ! -t 0 ]; then
+    AUTO="true"
+  fi
+
+  parse_arguments "$@"
+
+  setup_basics
 
   step_user_creation
+  step_ufw
+  step_nginx
   step_final
 
 }
