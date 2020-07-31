916360c6560d8611726bbe297f42771aba8bf0a2
Hélio <insign@gmail.com>
2020-07-31 03:58:32 -0300

    Fix for nginx - avoid rm errors when not exist folder

--- install.sh
+++ install.sh
@@ -402,7 +402,7 @@ EOF
 
       openssl dhparam -out /etc/nginx/dhparams.pem 2048
 
-      rm /etc/nginx/sites-{available,enabled}/default
+      rm -rf /etc/nginx/sites-{available,enabled}/default
 
       cat >/etc/nginx/sites-available/catch-all <<EOF
 server {
